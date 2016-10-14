e8e4b06d569a34f3cbeec0d7fe40f8a5dfb18a3d
Ivan <Ivan@MALENA>
2016-10-13 21:28:03 -0500

    Se agregó logo al login de AdminPC y cambio de nombres de las variables

--- AdminPC/WinFormApp/Login.Designer.cs
+++ AdminPC/WinFormApp/Login.Designer.cs
@@ -29,83 +29,96 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lbUsr = new System.Windows.Forms.Label();
+            this.lbPasw = new System.Windows.Forms.Label();
+            this.txtUsr = new System.Windows.Forms.TextBox();
+            this.txtPasw = new System.Windows.Forms.TextBox();
+            this.btnRegis = new System.Windows.Forms.Button();
+            this.btnIngre = new System.Windows.Forms.Button();
+            this.lnkContra = new System.Windows.Forms.LinkLabel();
+            this.logo_adpc = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_adpc)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lbUsr
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(304, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Usuario:";
+            this.lbUsr.AutoSize = true;
+            this.lbUsr.BackColor = System.Drawing.Color.Transparent;
+            this.lbUsr.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsr.Location = new System.Drawing.Point(276, 58);
+            this.lbUsr.Name = "lbUsr";
+            this.lbUsr.Size = new System.Drawing.Size(72, 18);
+            this.lbUsr.TabIndex = 0;
+            this.lbUsr.Text = "Usuario:";
             // 
-            // label2
+            // lbPasw
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(276, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Contraseña:";
+            this.lbPasw.AutoSize = true;
+            this.lbPasw.BackColor = System.Drawing.Color.Transparent;
+            this.lbPasw.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPasw.Location = new System.Drawing.Point(276, 106);
+            this.lbPasw.Name = "lbPasw";
+            this.lbPasw.Size = new System.Drawing.Size(100, 18);
+            this.lbPasw.TabIndex = 1;
+            this.lbPasw.Text = "Contraseña:";
             // 
-            // textBox1
+            // txtUsr
             // 
-            this.textBox1.Location = new System.Drawing.Point(396, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtUsr.Location = new System.Drawing.Point(396, 58);
+            this.txtUsr.Name = "txtUsr";
+            this.txtUsr.Size = new System.Drawing.Size(212, 20);
+            this.txtUsr.TabIndex = 2;
             // 
-            // textBox2
+            // txtPasw
             // 
-            this.textBox2.Location = new System.Drawing.Point(396, 107);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(212, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtPasw.Location = new System.Drawing.Point(396, 107);
+            this.txtPasw.Name = "txtPasw";
+            this.txtPasw.Size = new System.Drawing.Size(212, 20);
+            this.txtPasw.TabIndex = 3;
             // 
-            // button1
+            // btnRegis
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(396, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Registrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRegis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegis.Location = new System.Drawing.Point(396, 146);
+            this.btnRegis.Name = "btnRegis";
+            this.btnRegis.Size = new System.Drawing.Size(75, 28);
+            this.btnRegis.TabIndex = 4;
+            this.btnRegis.Text = "Registrar";
+            this.btnRegis.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnIngre
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(533, 146);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Ingresar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnIngre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngre.Location = new System.Drawing.Point(533, 146);
+            this.btnIngre.Name = "btnIngre";
+            this.btnIngre.Size = new System.Drawing.Size(75, 28);
+            this.btnIngre.TabIndex = 5;
+            this.btnIngre.Text = "Ingresar";
+            this.btnIngre.UseVisualStyleBackColor = true;
             // 
-            // linkLabel1
+            // lnkContra
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(357, 238);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(186, 16);
-            this.linkLabel1.TabIndex = 6;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "¿Has olvidado tu contraseña?";
+            this.lnkContra.AutoSize = true;
+            this.lnkContra.BackColor = System.Drawing.Color.Transparent;
+            this.lnkContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkContra.LinkColor = System.Drawing.Color.Black;
+            this.lnkContra.Location = new System.Drawing.Point(357, 238);
+            this.lnkContra.Name = "lnkContra";
+            this.lnkContra.Size = new System.Drawing.Size(186, 16);
+            this.lnkContra.TabIndex = 6;
+            this.lnkContra.TabStop = true;
+            this.lnkContra.Text = "¿Has olvidado tu contraseña?";
+            // 
+            // logo_adpc
+            // 
+            this.logo_adpc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logo_adpc.Image = ((System.Drawing.Image)(resources.GetObject("logo_adpc.Image")));
+            this.logo_adpc.Location = new System.Drawing.Point(23, 60);
+            this.logo_adpc.Name = "logo_adpc";
+            this.logo_adpc.Size = new System.Drawing.Size(247, 149);
+            this.logo_adpc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo_adpc.TabIndex = 7;
+            this.logo_adpc.TabStop = false;
             // 
             // Login
             // 
@@ -114,15 +127,18 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(653, 273);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logo_adpc);
+            this.Controls.Add(this.lnkContra);
+            this.Controls.Add(this.btnIngre);
+            this.Controls.Add(this.btnRegis);
+            this.Controls.Add(this.txtPasw);
+            this.Controls.Add(this.txtUsr);
+            this.Controls.Add(this.lbPasw);
+            this.Controls.Add(this.lbUsr);
             this.Name = "Login";
+            this.ShowIcon = false;
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.logo_adpc)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,12 +146,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lbUsr;
+        private System.Windows.Forms.Label lbPasw;
+        private System.Windows.Forms.TextBox txtUsr;
+        private System.Windows.Forms.TextBox txtPasw;
+        private System.Windows.Forms.Button btnRegis;
+        private System.Windows.Forms.Button btnIngre;
+        private System.Windows.Forms.LinkLabel lnkContra;
+        private System.Windows.Forms.PictureBox logo_adpc;
     }
 }
